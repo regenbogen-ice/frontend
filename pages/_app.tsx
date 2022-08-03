c7c9d4f9373a7647119de6e9fb5f168234beea61
Philipp PplusS <pplussinfo@gmail.com>
2022-08-03 19:19:25 +0200

    add more metatags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -34,6 +34,10 @@ export default function App({ Component, pageProps }: AppProps) {
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet" />
+                <link rel="icon" type="image/png" href="/images/icon-bg-32.png" />
+                <link rel='manifest' href='/manifest.json' crossOrigin='use-credentials' />
+                <link rel="apple-touch-icon" href="/images/icon-bg-400.png" />
+                <meta name="mobile-web-app-capable" content="yes" />
             </Head>
             <Component {...pageProps} />
             <GlobalStyles />
